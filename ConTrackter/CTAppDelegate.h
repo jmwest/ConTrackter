1170b31a3ca11f9f1ebb0b31a623bda0a6d63c2a
JohnWest <johnwest@umich.edu>
2013-05-17 14:22:01 -0400

    Initial Commit

---
+++ ConTrackter/CTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CTAppDelegate.h
+//  ConTrackter
+//
+//  Created by John West on 5/17/13.
+//  Copyright (c) 2013 John West. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
